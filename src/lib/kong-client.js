d5489e9bd929db0292c28ed752129c0cd08447ce
Darren Jennings <dmjenn02@gmail.com>
2019-06-02 00:29:06 -0700

    wip

--- src/lib/kong-client.js
+++ src/lib/kong-client.js
@@ -1,56 +1,48 @@
-// import DFetch from 'digest-fetch';
-// import { Response } from 'node-fetch';
-// import { FetchOptions } from '../types';
+import DFetch from 'digest-fetch';
 
-// interface ClientOptions {
-//   username: string;
-//   apiKey: string;
-// }
+export default class KongClient {
+  constructor (options) {
+    this.options = options;
+    this.client = new DFetch(options.url, options.apiKey);
+  }
 
-// export default class AtlasClient {
-//   options: ClientOptions;
-//   client: any;
+  fetch (path, options) {
+    const apiUrl = `${this.options.url}${path}`;
+    console.log(apiUrl)
+    if (options.data) {
+      options.headers = {
+        ...options.headers,
+        'Content-Type': 'application/json'
+      };
+      options.body = JSON.stringify(options.data);
+    }
 
-//   constructor(options: ClientOptions) {
-//     this.options = options;
-//     this.client = new DFetch(options.username, options.apiKey);
-//   }
+    return this.client.fetch(apiUrl, options);
+  }
 
-//   fetch(path: string, options: FetchOptions) {
-//     const apiUrl = `https://cloud.mongodb.com/api/atlas/v1.0${path}`;
-//     if (options.data) {
-//       options.headers = {
-//         ...options.headers,
-//         'Content-Type': 'application/json'
-//       };
-//       options.body = JSON.stringify(options.data);
-//     }
+  async fetchAndThrow (path, options) {
+    const res = await this.fetch(path, options);
+    if (res.status !== 200) {
+      throw new Error(
+        `Failed Kong API call. path: ${path} status: ${
+        res.status
+        } error: ${await res.text()}`
+      );
+    }
 
-//     return this.client.fetch(apiUrl, options) as Promise<Response>;
-//   }
+    const response = await res.json();
 
-//   async fetchAndThrow(path: string, options: FetchOptions) {
-//     const res = await this.fetch(path, options);
-//     if (res.status !== 200) {
-//       throw new Error(
-//         `Failed Atlas API call. path: ${path} status: ${
-//           res.status
-//         } error: ${await res.text()}`
-//       );
-//     }
+    return response;
+  }
 
-//     const response = await res.json();
-//     // Handling a list
-//     // TODO: Implement pagination support.
-//     if (response.links && response.results) {
-//       return response.results;
-//     }
-
-//     return response;
-//   }
-
-//   async authCheck() {
-//     const res = await this.fetch('/', { method: 'GET' });
-//     return res.status === 200;
-//   }
-// }
+  async authCheck () {
+    let res 
+    try{
+      res = await this.fetch('/', { method: 'GET' });
+    } catch (e) {
+      console.log(e)
+    }
+    
+    return res.status === 200;
+  }
+}
--- src/views/setup.js
+++ src/views/setup.js
@@ -1,35 +1,36 @@
-// import KongClient from '../lib/kong-client';
-// import {withUiHook, htm} from '@zeit/integration-utils';
+import KongClient from '../lib/kong-client';
 
 console.log("setupView")
-export default async function setupView(viewData) {
-    const { payload, metadata, zeitClient } = viewData;
-    const { kongAdminApiUrl, apiKey } = payload.clientState; // From form submition
-  
-    let error = null;
-    if (payload.action === 'setup') {
-      if (!kongAdminApiUrl || !apiKey) {
-        error = 'Both "Kong Admin URL" and "Kong API Key" are required.';
-      } else {
-        const kongClient = new KongClient({ kongAdminApiUrl, apiKey });
-        viewData.kongClient = kongClient;
-        const authChecked = await kongClient.authCheck();
-        if (authChecked) { // If the API key and URL are good, go to the next view
-          metadata.connectionInfo = { kongAdminApiUrl, apiKey };
-          await zeitClient.setMetadata(metadata);
-          return deploymentsView(viewData)
-        }
-  
-        error = 'Either "Kong URL" or "Kong API Key" is incorrect.';
+export default async function setupView (viewData) {
+  const { payload, metadata, zeitClient } = viewData;
+  const { kongAdminApiUrl, apiKey } = payload.clientState; // From form submition
+
+  let error = null;
+  if (payload.action === 'setup') {
+    if (!kongAdminApiUrl || !apiKey) {
+      error = 'Both "Kong Admin URL" and "Kong API Key" are required.';
+    } else {
+      console.log(kongAdminApiUrl)
+      const kongClient = new KongClient({ url: kongAdminApiUrl, apiKey });
+      viewData.kongClient = kongClient;
+      const authChecked = await kongClient.fetchAndThrow('', {});
+      if (authChecked) { // If the API key and URL are good, go to the next view
+        console.log(authChecked)
+        metadata.connectionInfo = { kongAdminApiUrl, apiKey };
+        await zeitClient.setMetadata(metadata);
+        return `
+        <UL>
+          ${Object.keys(authChecked.plugins.available_on_server).map(plugin => `<LI>${plugin}</LI>`)}
+        </UL>
+        `
+        // return deploymentsView(viewData)
       }
-}
 
+      error = 'Either "Kong URL" or "Kong API Key" is incorrect.';
+    }
+  }
 
-
-let kongAdminApiUrl = '';
-let apiKey = '';
-
-	return `
+  return `
         <Box>
             <Fieldset>
                 <H1>Kong API Integration</H1>
@@ -48,9 +49,9 @@ let apiKey = '';
                     <Input name="apiKey" value="${apiKey || ''}"/>
                 </FsContent>
             </Fieldset>
-            ${error ? htm`<Box color="red" marginBottom="20px">${error}</Box>`
-                : ''
-            }        
+            ${error ? `<Box color="red" marginBottom="20px">${error}</Box>`
+      : ''
+    }        
             <Button action="setup">Setup</Button>
 		</Box>
     `
