a9ae502438c88561d0d349961dcf77c28d50ee62
Darren Jennings <dmjenn02@gmail.com>
2019-06-02 16:28:50 -0700

    style(projects) project list style fixes

--- src/views/projects.js
+++ src/views/projects.js
@@ -1,3 +1,4 @@
+import { htm } from '@zeit/integration-utils'
 import setupView from './setup';
 import servicesView from './services';
 
@@ -13,16 +14,24 @@ export default async function projectsView (viewData) {
 
   let apiUrl = `/v1/projects/list`;
   const projects = await zeitClient.fetchAndThrow(apiUrl, { method: 'GET' });
-
+  const getTimestamp = (unix) => {
+    const date = new Date(unix)
+    return date.toUTCString();
+  }
+  
   return `
-  <Box>
-    <Fieldset>
-      <FsContent>
-        <H1>What project do you want to set up with Kong?</H1>
-      </FsContent>
-      <FsContent>
-        ${projects.map(d => `<Link action="choose-${d.id}" ><H2>${d.name}</H2></Link><BR />`)}
-      </FsContent>
-    </Fieldset>
-  </Box>`
+  <Page>
+    <H1>What project do you want to set up with Kong?</H1>
+    <BR/>
+    <Box border="1px solid" padding="1rem" borderRadius="5px" borderColor="hsla(205, 100%, 72%, 1)">
+      <UL>
+        ${projects.map(d => `
+          <Container>
+            <Link action="choose-${d.id}">${d.name}</Link>
+            <Box color="hsla(0, 0%, 0%, 0.5)">Created: ${getTimestamp(d.createdAt)}, Last Updated: ${getTimestamp(d.updatedAt)}</Box>
+          </Container>`
+  ).join('')}
+      </UL>
+    </Box>
+  </Page>`
 }
